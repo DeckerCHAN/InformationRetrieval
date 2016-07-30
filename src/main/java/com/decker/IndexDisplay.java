c982c7274da79694afebc4b33c85a4c625088fe1
DeckerCHAN <DeckerCHAN@gmail.com>
2016-07-30 13:29:38 +1000

    Re-format codes.

--- src/main/java/com/decker/IndexDisplay.java
+++ src/main/java/com/decker/IndexDisplay.java
@@ -1,12 +1,10 @@
-/** Dump index contents to output stream.
- * 
+/**
+ * Dump index contents to output stream.
+ *
  * @author Scott Sanner, Paul Thomas
  */
 
 package com.decker;
-
-import java.io.PrintStream;
-import java.nio.file.Paths;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -15,32 +13,35 @@ import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.SimpleFSDirectory;
 
+import java.io.PrintStream;
+import java.nio.file.Paths;
+
 public class IndexDisplay {
 
-	public static void Display(String index_path, PrintStream ps) 
-		throws Exception {
-		Directory d = new SimpleFSDirectory(Paths.get(index_path));
-		DirectoryReader r = DirectoryReader.open(d);
-		Display(r, ps);
-		r.close();
-	}
+    public static void Display(String index_path, PrintStream ps)
+            throws Exception {
+        Directory d = new SimpleFSDirectory(Paths.get(index_path));
+        DirectoryReader r = DirectoryReader.open(d);
+        Display(r, ps);
+        r.close();
+    }
 
-	public static void Display(IndexReader r, PrintStream ps) 
-		throws Exception {
-		
-		for (int d = 0; d < r.maxDoc(); d++) {
-			ps.println("=========================================");
-			Document doc = r.document(d);
-			for (Object o : doc.getFields()) {
-				Field f = (Field)o;
-				ps.println(f.name() + ": " + f.stringValue());
-			}
-		}
-		ps.println("=========================================");
-	}
-	
-	public static void main(String[] args) throws Exception{
-		Display("src/web/search/lucene.index", System.out);
-	}
+    public static void Display(IndexReader r, PrintStream ps)
+            throws Exception {
+
+        for (int d = 0; d < r.maxDoc(); d++) {
+            ps.println("=========================================");
+            Document doc = r.document(d);
+            for (Object o : doc.getFields()) {
+                Field f = (Field) o;
+                ps.println(f.name() + ": " + f.stringValue());
+            }
+        }
+        ps.println("=========================================");
+    }
+
+    public static void main(String[] args) throws Exception {
+        Display("src/web/search/lucene.index", System.out);
+    }
 
 }
